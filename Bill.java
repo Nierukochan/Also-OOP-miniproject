59e771b4826b85d313851d6c4b09c239c9d829e1
Nierukochan <sirimongkol58@gmail.com>
2024-03-02 00:25:32 +0700

    eighth commit

--- Bill.java
+++ Bill.java
@@ -1,32 +1,72 @@
-public class Bill {
-  
-  public int bill_no;
-  public double bill_vat;
+public class Bill extends Order {
 
-  public Bill() {}
+  public String bill_no;
+  public double vat;
+  public double total;
+  public double net;
 
-  public Bill(int bill_no ,double bill_vat) {
+  public Bill() {
+  }
+
+  public Bill(String bill_no, double bill_vat) {
     this.bill_no = bill_no;
-    this.bill_vat = bill_vat;
+    this.vat = vat;
   }
 
-  public void SetBillno(int bill_no) {
+  public Bill(String order_no,
+      String order_date, String bill_no, double vat) {
+    super(order_no, order_date);
+    this.bill_no = bill_no;
+    this.vat = vat;
+  }
+
+  public void setBillno(String bill_no) {
     this.bill_no = bill_no;
   }
 
-  public void SetBillVat(double bill_vat) {
-    this.bill_vat = bill_vat;
-  }
-
-  public int getBillno() {
+  public String getBillno() {
     return bill_no;
   }
 
-  public double getBillVat() {
-    return bill_vat;
+  public void setVat(double vat) {
+    this.vat = vat;
   }
 
-  public void getBill() {
-    
+  public double getVat() {
+    return vat;
+  }
+
+  public void setTotal(double total) {
+    this.total = total;
+  }
+
+  public double getTotal() {
+    return total;
+  }
+
+  public void setNet(double net) {
+    this.net = net;
+  }
+
+  public double getNet() {
+    return net;
+  }
+
+  public void callvat_total(Bill bill) {
+    net = bill.getTotal() - bill.getVat();
+    bill.setNet(net);
+  }
+
+  public void callVat(Bill bill) {
+    vat = bill.getTotal() * 7 / 100;
+    bill.setVat(vat);
+  }
+
+  public void printInfo() {
+    super.printInfo();
+    System.out.println("Order vat : " + vat);
+    System.out.println("Order total : " + total);
+    System.out.println("Ornet Net: " + net);
+
   }
 }
--- Calemp.java
+++ Calemp.java
@@ -12,19 +12,19 @@ import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 
-public class Calemp { 
+public class Calemp {
 
     public static void Emp() {
 
         Scanner input = new Scanner(System.in);
         System.out.println("Enter your Employee ID...");
         String empno_input = input.nextLine();
-        //String emp_name;
+        // String emp_name;
 
         JSONParser parser = new JSONParser();
 
         try {
-            
+
             Reader readEmp = new FileReader("./employee.json");
             JSONArray empArray = (JSONArray) parser.parse(readEmp);
 
@@ -33,15 +33,45 @@ public class Calemp {
                 JSONObject objectEmployee = iterator.next();
 
                 if (objectEmployee != null && empno_input.equals(String.valueOf(objectEmployee.get("Emp_no")))) {
-                   
+
                     Employee emp = new Employee();
                     emp.setName((String) objectEmployee.get("Emp_name"));
-                    emp.setPnumber((String)objectEmployee.get("Emp_tel"));
-                    emp.setEmpno((String)objectEmployee.get("Emp_no"));
-                    ViewOrder();
+                    emp.setPnumber((String) objectEmployee.get("Emp_tel"));
+                    emp.setEmpno((String) objectEmployee.get("Emp_no"));
+                    // ViewOrder();
+
+                    while (true) {
+                        System.out.println("=================== Employee ===================");
+                        System.out.println("1: Pay transaction");
+                        System.out.println("2: Bill History");
+                        System.out.println("3: Return main menu");
+                        System.out.println("___________________________");
+                        System.out.print("Enter your List : ");
+
+                        String choice = input.next();
+
+                        if (choice.equalsIgnoreCase("1")) {
+                            // clearConsole();
+                            ViewOrder();
+
+                        } else if (choice.equalsIgnoreCase("2")) {
+                            // Confirm conn = new Confirm();
+                            BillHistory();
+                        } else if (choice.equalsIgnoreCase("3")) {
+                            // clearConsole();
+                            Index indexpage = new Index();
+                            indexpage.Mainpage();
+                            break;
+                            // System.out.println("Exiting the program. Goodbye!");
+                            // System.exit(0);
+                        } else {
+                            System.out.println("Invalid choice. Please try again.");
+
+                        }
+
+                    }
 
                 }
-
             }
 
         } catch (IOException e) {
@@ -55,14 +85,13 @@ public class Calemp {
 
     public static void ViewOrder() {
 
-        String emp_no, select_phone ,phone,table_no;
+        String emp_no, select_phone, phone, table_no;
         String status = "Confirm";
         String newStatus = "Checked";
-        //String itemconfirmed = "Confirmed_Items";
+        // String itemconfirmed = "Confirmed_Items";
         double sumprice = 0.0;
         int i = 1;
         Scanner input = new Scanner(System.in);
-
 
         System.out.println("===================View Order===================");
 
@@ -77,32 +106,30 @@ public class Calemp {
             while (iterator.hasNext()) {
                 JSONObject objectOrder = iterator.next();
 
-
                 table_no = (String) objectOrder.get("Customer_Table_No");
                 phone = (String) objectOrder.get("Customer_Phone_name");
 
-                if(status.equals(String.valueOf(objectOrder.get("Customer_Confirm_Status")))) {
-                     System.out.println("Table No: " + table_no);
-                     System.out.println("Customer phone number: " + phone);
-                     System.out.println("_______________________________");
+                if (status.equals(String.valueOf(objectOrder.get("Customer_Confirm_Status")))) {
+                    System.out.println("Table No: " + table_no);
+                    System.out.println("Customer phone number: " + phone);
+                    System.out.println("_______________________________");
                 }
             }
 
-                    
             System.out.println("==================Select Order==================");
             System.out.print("Enter Phone number for create bill : ");
             select_phone = input.nextLine();
             System.out.println("Customer Tel : " + select_phone);
             System.out.println("===================== " + select_phone + " =====================");
 
-            JSONArray nbillArray = new JSONArray(); 
+            JSONArray nbillArray = new JSONArray();
 
             for (Object obj : orderArray) {
                 JSONObject objectOrder = (JSONObject) obj;
                 JSONArray itemList = (JSONArray) objectOrder.get("Confirmed_Items");
 
-
-                if (objectOrder != null && select_phone.equals(String.valueOf(objectOrder.get("Customer_Phone_name")))) {
+                if (objectOrder != null
+                        && select_phone.equals(String.valueOf(objectOrder.get("Customer_Phone_name")))) {
 
                     JSONObject billObject = new JSONObject();
 
@@ -114,7 +141,6 @@ public class Calemp {
                     billObject.put("table", objectOrder.get("Customer_Table_No"));
                     billObject.put("Date", objectOrder.get("Customer_Table_Date"));
 
-
                     for (Object item : itemList) {
                         JSONObject itemObject = (JSONObject) item;
                         System.out.println(i + " Order: " + itemObject.get("Product_Name") + " : "
@@ -123,12 +149,12 @@ public class Calemp {
                         i += 1;
                     }
                     billObject.put("Total", sumprice);
-                    billObject.put("Vat(7%)",(Double)(sumprice/100)*7.00);
+                    billObject.put("Vat(7%)", (Double) (sumprice / 100) * 7.00);
                     nbillArray.add(billObject);
                 }
 
             }
-            System.out.println("Total : "+sumprice);
+            System.out.println("Total : " + sumprice);
             System.out.println("====================== End ======================");
 
             try (FileWriter fileWriter = new FileWriter("./confirmOrder.json")) {
@@ -139,34 +165,82 @@ public class Calemp {
                 e.printStackTrace();
             }
 
+            try (FileReader reader = new FileReader("./bill.json")) {
+                JSONArray existingBillArray = (JSONArray) parser.parse(reader);
 
-                try (FileReader reader = new FileReader("./bill.json")) {
-                    JSONArray existingBillArray = (JSONArray) parser.parse(reader);
+                // Append new bill data
+                existingBillArray.addAll(nbillArray);
 
-                    // Append new bill data
-                    existingBillArray.addAll(nbillArray);
-
-                    // Write back to bill.json
-                    try (FileWriter file = new FileWriter("./bill.json")) {
-                        file.write(existingBillArray.toJSONString());
-                        System.out.println("Insert Data To Json Successfully");
-                    } catch (IOException e) {
-                        System.out.println("Handle exception cannot find file");
-                        e.printStackTrace();
-                    }
+                // Write back to bill.json
+                try (FileWriter file = new FileWriter("./bill.json")) {
+                    file.write(existingBillArray.toJSONString());
+                    System.out.println("Insert Data To Json Successfully");
                 } catch (IOException e) {
-                    // TODO: handle exception
-                } catch (ParseException e) {
-
+                    System.out.println("Handle exception cannot find file");
+                    e.printStackTrace();
                 }
-
-
             } catch (IOException e) {
                 // TODO: handle exception
             } catch (ParseException e) {
 
             }
 
+        } catch (IOException e) {
+            // TODO: handle exception
+        } catch (ParseException e) {
+
+        }
+
     }
+
+    public static void BillHistory() {
+
+        JSONParser parser = new JSONParser();
+        try {
+            Reader reader = new FileReader("./bill.json");
+            JSONArray billArray = (JSONArray) parser.parse(reader);
+
+            Iterator<JSONObject> iterator = billArray.iterator();
+            while (iterator.hasNext()) {
+                JSONObject billObject = iterator.next();
+
+                // Accessing properties within each billObject
+                String billNo = (String) billObject.get("Bill_No");
+                String customerName = (String) billObject.get("Customer_Name");
+                String tableNo = (String) billObject.get("Customer_Table_No");
+                double billTotal = (double) billObject.get("Bill_Total");
+
+                // You can access other properties in a similar way
+
+                // Process the data as needed
+                System.out.println("Bill No: " + billNo);
+                System.out.println("Customer Name: " + customerName);
+                System.out.println("Table No: " + tableNo);
+                System.out.println("Bill Total: " + billTotal);
+                
+                // Now you can access and process the "Order" array
+                JSONArray orderArray = (JSONArray) billObject.get("Order");
+                Iterator<JSONObject> orderIterator = orderArray.iterator();
+                while (orderIterator.hasNext()) {
+                    JSONObject orderItem = orderIterator.next();
+
+                    // Access properties within each orderItem
+                    double productPrice = (double) orderItem.get("Product_Price");
+                    String productName = (String) orderItem.get("Product_Name");
+
+                    // Process the order data as needed
+                    System.out.println("Product Name: " + productName);
+                    System.out.println("Product Price: " + productPrice);
+                }
+                System.out.println("-------------------");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace(); // Print the exception for debugging
+        } catch (ParseException e) {
+            
+        }
+    }
+
 
 }
